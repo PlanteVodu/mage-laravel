317997fd7d053c0478c823c8356d4e6780ce9ecb
FildasPA <fildas.pa3@gmail.com>
2019-12-29 14:57:23 +0100

    Refactor ActorReferenceTest

--- tests/Feature/Actor/ActorReferenceTest.php
+++ tests/Feature/Actor/ActorReferenceTest.php
@@ -12,54 +12,45 @@ class ActorReferenceTest extends TestCase
 {
     use RefreshDatabase;
 
-    public static function data($array = [])
+    protected function setUp() : void
     {
-        $data = [
-            'name' => 'Cool name',
-            'note' => 'Some notes',
-        ];
-
-        return array_merge($data, $array);
+        parent::setUp();
+        factory(Reference::class, 5)->create();
     }
 
     public function test_references_can_be_added()
     {
-        $this->post('/references', ReferenceTest::data());
-        $this->post('/references', ReferenceTest::data(['name' => 'Another name']));
-
-        $data = self::data(['references' => Reference::all()->modelKeys()]);
-        $response = $this->post('/actors', $data);
+        $actor = factory(Actor::class)->make(['references' => Reference::all()->modelKeys()]);
+        $response = $this->post('/actors', $actor->toArray());
         $response->assertOk();
         $this->assertCount(1, Actor::all());
-        $this->assertCount(2, Actor::first()->references);
+        $this->assertCount(5, Actor::first()->references);
         $this->assertEquals(Reference::first()->id, Actor::first()->references[0]->id);
 
-        $this->post('/actors', self::data(['references' => Reference::first()->getKey()]));
+        $actor = factory(Actor::class)->make(['references' => Reference::first()->getKey()]);
+        $this->post('/actors', $actor->toArray());
         $response->assertOk();
         $this->assertCount(2, Actor::all());
-        $this->assertCount(2, Actor::find(1)->references);
+        $this->assertCount(5, Actor::find(1)->references);
         $this->assertCount(1, Actor::find(2)->references);
-        $this->assertEquals(1, Actor::first()->references[0]->id);
-        $this->assertEquals(2, Actor::first()->references[1]->id);
         $this->assertEquals(1, Actor::find(2)->references[0]->id);
     }
 
     public function test_references_can_be_removed()
     {
-        $this->post('/references', ReferenceTest::data());
-        $this->post('/references', ReferenceTest::data(['name' => 'Another name']));
+        $actor = factory(Actor::class)->make(['references' => Reference::all()->modelKeys()]);
+        $this->post('/actors', $actor->toArray());
+        $actor = factory(Actor::class)->make(['references' => 2]);
+        $this->post('/actors', $actor->toArray());
 
-        $this->post('/actors', self::data(['references' => Reference::all()->modelKeys()]));
-        $this->post('/actors', self::data(['references' => 2]));
-
-        $actor = Actor::first();
-
-        $response = $this->patch('/actors/1', self::data(['references' => [Reference::first()->getKey()]]));
+        $actor = factory(Actor::class)->make(['references' => 1]);
+        $response = $this->patch('/actors/1', $actor->toArray());
         $response->assertOk();
         $this->assertCount(1, Actor::find(1)->references);
         $this->assertCount(1, Actor::find(2)->references);
 
-        $response = $this->patch('/actors/1', self::data(['references' => []]));
+        $actor = factory(Actor::class)->make(['references' => '']);
+        $response = $this->patch('/actors/1', $actor->toArray());
         $response->assertOk();
         $this->assertCount(0, Actor::find(1)->references);
         $this->assertCount(1, Actor::find(2)->references);
@@ -67,10 +58,8 @@ class ActorReferenceTest extends TestCase
 
     public function test_references_must_be_distinct()
     {
-        $this->post('/references', ReferenceTest::data());
-
-        $data = self::data(['references' => [1, 1]]);
-        $response = $this->post('/actors', $data);
+        $actor = factory(Actor::class)->make(['references' => [1,1]]);
+        $response = $this->post('/actors', $actor->toArray());
         $response->assertSessionHasErrors('references.0', 'references.1');
         $this->assertCount(0, Actor::all());
     }
