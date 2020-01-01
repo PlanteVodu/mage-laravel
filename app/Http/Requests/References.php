90bf1fc8bf6352f99c12f55e1625aa121cc36fb5
FildasPA <fildas.pa3@gmail.com>
2020-01-01 20:52:42 +0100

    References Request handle nested arrays
    Validation rules applied on nested arrays are applied globally. I wanted it
    to be applied on each deepest array separately for ActorKinships References
    (the rule was : kinships.*.references.*).
    
    Let's take an example. An Actor can have several ActorKinships, and each
    ActorKinship can have several References. References have to be different for
    each ActorKinship, but References can reference several ActorKinships at once.
    To ensure References were different, we used to apply a validation rule like
    this :
    
    kinships.*.references.* => 'distinct'
    
    But the 'distinct' rule is applied globally, so this array will not pass :
    
    [
      'kinships' => [
        [ 'references' => [1, 2] ],
        [ 'references' => [1] ],
      ]
    ]
    
    because '1' appears more than once inside the WHOLE array. We wish to have it
    applied on each 'references' separately.
    
    I searched how to create custom validation rules in the Laravel docs
    (https://laravel.com/docs/6.x/validation#custom-validation-rules).
    I found several ways to do it, using either a custom Rule, or using a Service
    Provider. But the best solution in my opinion was to loop over every nested
    array and to apply the rules on every one of them separatly. This is not the
    perfect solution, as we have to loop over the Request fields and add rules
    dynamically, but fortunatly, it allows us to use the default 'distinct' rules
    on it.

--- app/Http/Requests/References.php
+++ app/Http/Requests/References.php
@@ -1,10 +1,16 @@
 <?php
 namespace App\Http\Requests;
 
+use Illuminate\Support\Str;
+
 abstract class References
 {
     public static function rules($prefix = '')
     {
+        if (Str::endsWith($prefix, '.*')) {
+            return self::getNestedArraysRules($prefix);
+        }
+
         if (strlen($prefix) > 0) {
             $prefix = $prefix . '.';
         }
@@ -16,5 +22,18 @@ abstract class References
                 'exists:references,id',
             ],
         ];
+    }
+
+    /**
+     *  Handle nested arrays, ensuring the rules are applied separately on each
+     *  of the deepest arrays.
+     */
+    protected static function getNestedArraysRules($prefix)
+    {
+        $prefix = Str::beforeLast($prefix, '.*');
+        $fields = collect(request()->get($prefix));
+        return $fields->map(function ($item, $key) use ($prefix) {
+            return self::rules($prefix . '.' . $key);
+        })->toArray();
     }
 }
