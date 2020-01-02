a229c2972f6439b0e82e24d4a769a8df1fe998c7
FildasPA <fildas.pa3@gmail.com>
2020-01-02 12:44:39 +0100

    Refactor ActorReferenceTest

--- tests/Feature/Actor/ActorReferenceTest.php
+++ tests/Feature/Actor/ActorReferenceTest.php
@@ -18,17 +18,24 @@ class ActorReferenceTest extends TestCase
         factory(Reference::class, 5)->create();
     }
 
+    public function getActor(...$references)
+    {
+        return factory(Actor::class)
+            ->make(['references' => $references])
+            ->toArray();
+    }
+
     public function test_references_can_be_added()
     {
-        $actor = factory(Actor::class)->make(['references' => Reference::all()->modelKeys()]);
-        $response = $this->post('/actors', $actor->toArray());
+        $actor = $this->getActor(...Reference::all()->modelKeys());
+        $response = $this->post('/actors', $actor);
         $response->assertOk();
         $this->assertCount(1, Actor::all());
         $this->assertCount(5, Actor::first()->references);
         $this->assertEquals(Reference::first()->id, Actor::first()->references[0]->id);
 
-        $actor = factory(Actor::class)->make(['references' => Reference::first()->getKey()]);
-        $this->post('/actors', $actor->toArray());
+        $actor = $this->getActor(Reference::first()->id);
+        $this->post('/actors', $actor);
         $response->assertOk();
         $this->assertCount(2, Actor::all());
         $this->assertCount(5, Actor::find(1)->references);
@@ -38,19 +45,19 @@ class ActorReferenceTest extends TestCase
 
     public function test_references_can_be_removed()
     {
-        $actor = factory(Actor::class)->make(['references' => Reference::all()->modelKeys()]);
-        $this->post('/actors', $actor->toArray());
-        $actor = factory(Actor::class)->make(['references' => 2]);
-        $this->post('/actors', $actor->toArray());
+        $actor = $this->getActor(...Reference::all()->modelKeys());
+        $this->post('/actors', $actor);
+        $actor = $this->getActor(2);
+        $this->post('/actors', $actor);
 
-        $actor = factory(Actor::class)->make(['references' => 1]);
-        $response = $this->patch('/actors/1', $actor->toArray());
+        $actor = $this->getActor(1);
+        $response = $this->patch('/actors/1', $actor);
         $response->assertOk();
         $this->assertCount(1, Actor::find(1)->references);
         $this->assertCount(1, Actor::find(2)->references);
 
-        $actor = factory(Actor::class)->make(['references' => '']);
-        $response = $this->patch('/actors/1', $actor->toArray());
+        $actor = $this->getActor();
+        $response = $this->patch('/actors/1', $actor);
         $response->assertOk();
         $this->assertCount(0, Actor::find(1)->references);
         $this->assertCount(1, Actor::find(2)->references);
@@ -58,8 +65,8 @@ class ActorReferenceTest extends TestCase
 
     public function test_references_must_be_distinct()
     {
-        $actor = factory(Actor::class)->make(['references' => [1,1]]);
-        $response = $this->post('/actors', $actor->toArray());
+        $actor = $this->getActor(1, 1);
+        $response = $this->post('/actors', $actor);
         $response->assertSessionHasErrors('references.0', 'references.1');
         $this->assertCount(0, Actor::all());
     }
